f5200fb67ff5b9a4cba55b00455490d4aaaa610a
Jalal El Zein <jalal13elzein@gmail.com>
2026-07-26 17:48:30 +0100

    accept configuration with argparse

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 from src.run_exp import exp_centralized,  exp_centralized_for
 from src.solver import QUBO_solver
+import argparse
 import json
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', type=str, default='configs/Partitioning_new.json',
+                     help='Path to the JSON config file')
+args = parser.parse_args()
 
-with open('configs/Partitioning_new.json') as f:
+with open(args.config) as f:
    params = json.load(f)
 exp_centralized(params)
 
